0ae30953d7c08aa85b36f2eb67600e6135a22148
renzaspiras <aspirasrenz@gmail.com>
2023-02-17 18:56:22 +0800

    kkk

--- pack/final.sh
+++ pack/final.sh
@@ -1,4 +1,4 @@
-cp asset/update.sh /data/data/com.termux/files/usr/bin/update.sh
+cp pack/asset/update.sh /data/data/com.termux/files/usr/bin/update.sh
 chmod +x /data/data/com.termux/files/usr/bin/update.sh
 
 printf "\nAll task is finished!\n"
