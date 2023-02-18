5707473c6fc6407557e6228d25ff121dc23be9b3
renzaspiras <aspirasrenz@gmail.com>
2023-02-18 20:20:11 +0800

    No more parameters

--- pack/phase3.sh
+++ pack/phase3.sh
@@ -11,13 +11,11 @@ npm install -g neovim
 
 printf "\nConfiguring Neovim like VS Code\n"
 #lua script
-#cp -r pack/asset/.config ~/.config
-#cp -r pack/asset/.config/* ~/.config/
 mkdir /data/data/com.termux/files/home/.config
 cp pack/asset/.config/nvim.tar.gz ~/.config/
 #tar xzvf  ~/.config/nvim.tar.gz
 tar xzvf ~/.config/nvim.tar.gz -C ~/.config/
-#rm ~/.config/nvim.tar.gz
+rm ~/.config/nvim.tar.gz
 
 #If it is finished?
 printf "\nPhase 3 Finished\n"
